651eeb9038fd33a942712859e5fb01ce7bd4b116
RayVenn <wenzhenyu36@gmail.com>
2016-04-16 12:27:11 -0400

    add argument for keyname when creating

--- create_ec2_instance.py
+++ create_ec2_instance.py
@@ -5,9 +5,13 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--name", help="define the instance name",
-        type=str, required=True, default=1)
+        type=str, required=True)
+parser.add_argument("-k", "--key", help="choose a key for this new instance",
+        tyep=str, required=True)
+
 args = parser.parse_args()
 name = args.name
+key  = args.key
 
 s = session.create_session()
 ec2 = s.resource('ec2')
@@ -15,7 +19,7 @@ ec2 = s.resource('ec2')
 instances = ec2.create_instances(
         ImageId='ami-d05e75b8',
         InstanceType='t2.micro',
-        KeyName = 'myself',
+        KeyName = key,
         SecurityGroupIds=['sg-eb8a4a92'],
         MinCount=1, MaxCount=1
         )
