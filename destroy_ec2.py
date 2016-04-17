7b71a448173df433c29f77be14e2844c836eb47b
RayVenn <wenzhenyu36@gmail.com>
2016-04-17 01:38:59 -0400

    add destroy instance

--- destroy_ec2.py
+++ destroy_ec2.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
-import session
 import list_ec2
+import parse_ec2
+import session
 
-s = session.create_session()
+instances_list = list_ec2.list_instances()
+
+choose_no = input('Choose an instance to terminate: ')
+choose_instance = instances_list[choose_no-1]
+choose_id = choose_instance['instance-id']
+
+ec2_config = parse_ec2.get_config('ec2.json')
+
+s = session.create_session(ec2_config['region'])
+
 ec2 = s.client('ec2')
-
-ec2.terminate_instances(InstanceIds=['i-40d109dd'])
+ec2.terminate_instances(InstanceIds=[choose_id])
